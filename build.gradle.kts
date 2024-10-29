8b59e44aff4aedbca73f069ba7ed7b1b1b7c9e27
Raphael Deschenes <deschenesr545@gmail.com>
2024-10-29 16:26:42 -0400

    Working workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,21 @@ repositories {
 dependencies {}
 
 publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.raphdf201"
+            artifactId = "lib"
+            version = "0.1"
+
+            from(components["java"])
+        }
+    }
     repositories {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/raphdf201/lib")
             credentials {
-                username = System.getenv("USERNAME")
+                username = "Raphdf201"
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
